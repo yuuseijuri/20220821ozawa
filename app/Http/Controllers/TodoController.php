32c0519a93d39c33eeb6e042a018a338addc6e22
takumiozawa <yuusei.1107.0121@gmail.com>
2022-08-23 16:44:16 +0900

    修正1

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,28 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\models\Author;
+use App\models\Todo;
 use App\Http\Requests\AuthorRequest;
 
 class TodoController extends Controller
 {
     public function index() {
-        $authors = Author::all();
+        $authors = Todo::all();
         return view('index', ['authors' => $authors]);
     }
     public function create(AuthorRequest $request) {
         $form = $request->task();
-        Author::create($form);
+        Todo::create($form);
         return redirect('/');
     }
     public function update(AuthorRequest $request) {
         $form = $request->task();
-        Author::where('id', $request->id)->update($form);
+        Todo::where('id', $request->id)->update($form);
         unset($form['_token']);
         return redirect('/');
     }
     public function remove(Request $request) {
-        Author::find($request->id)->delete();
+        Todo::find($request->id)->delete();
         return redirect('/');
     }
 }
